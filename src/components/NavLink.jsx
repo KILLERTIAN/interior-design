73f4ca83ed36b1f1d5ca749ab3a164ef855c31a2
Om Sharma <omsharma050322@gmail.com>
2023-11-08 22:42:17 +0530

    Nav update

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,7 +2,12 @@
 import { Link } from "react-scroll";
 
 const NavLink = ({ children, to, offset = -50, mobileMenu = false }) => {
-  return <div>NavLink</div>;
+  const className = mobileMenu ?
+    'text-lg text-black hover:text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2AF598_0%,_#009EFD_100%)] w-full text-center'
+    : 'text-lg lg:text-xl text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2AF598_0%,_#009EFD_100%)] hover:bg-clip-text hover:text-transparent'
+  return <Link to={to} spy={true} smooth={true} offset={offset} duration={1000} className={className}>
+    {children}
+  </Link>;
 };
 
 export default NavLink;
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -16,8 +16,28 @@ import { HiMenuAlt3 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return <div
+    className="h-screen relative flex flex-col items-center" style={{
+      background: `url(${heroImage})`,
+      backgroundPosition: 'bottom',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    }}
+  >
+    <div classname="relative w-full max-w-[1490px] flex items-center justify-between pt-10 mx-auto px-10">
+      <img src={logo} alt="" />
+      <ul classname="hidden md:flex items-center gap-10 lg:gap=[68px]">
+        <NavLink to="services">Services</NavLink>
+        <NavLink to="products">Shop</NavLink>
+        <NavLink to="reference">Reference</NavLink>
+        <NavLink to="care">Care</NavLink>
 
-};
+      </ul>
+
+    </div>
+
+  </div >;
+
+}
 
 export default Hero;
