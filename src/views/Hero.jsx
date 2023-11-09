5247e0f9e79669fc78dd66c7ae0f36482c071322
Om Sharma <omsharma050322@gmail.com>
2023-11-09 22:49:13 +0530

    updated content

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -16,27 +16,46 @@ import { HiMenuAlt3 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Hero = () => {
-  return <div
-    className="h-screen relative flex flex-col items-center" style={{
-      background: `url(${heroImage})`,
-      backgroundPosition: 'bottom',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-    }}
-  >
-    <div classname="relative w-full max-w-[1490px] flex items-center justify-between pt-10 mx-auto px-10">
-      <img src={logo} alt="" />
-      <ul classname="hidden md:flex items-center gap-10 lg:gap=[68px]">
-        <NavLink to="services">Services</NavLink>
-        <NavLink to="products">Shop</NavLink>
-        <NavLink to="reference">Reference</NavLink>
-        <NavLink to="care">Care</NavLink>
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-      </ul>
+  return (
+    <div
+      className="h-screen relative flex flex-col items-center" style={{
+        background: `url(${heroImage})`,
+        backgroundPosition: 'bottom',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="relative w-full max-w-[1490px] flex items-center justify-between pt-10 mx-auto px-10">
+        <img src={logo} alt="" />
+        <ul className="hidden md:flex items-center gap-10 lg:gap=[68px]">
+          <NavLink to="services">Services</NavLink>
+          <NavLink to="products">Shop</NavLink>
+          <NavLink to="reference">Reference</NavLink>
+          <NavLink to="care">Care</NavLink>
+        </ul>
+        <img src={cartIcon} alt="" className="hidden md:block cursor-pointer" />
+        <HiMenuAlt3 size={30} className="block md:hidden cursor-pointer text-white " onClick={() => setShowMobileMenu((prev) => !prev)} />
 
-    </div>
+        <div className={`block md:hidden w-full fixed ${!showMobileMenu ? "-top-[410px]" : "top-0"} left-0 bg-[#dde0e5] h-[410px] transition-all duration-[800ms] shadow-xl z-10 py-12 rounded-b-xl`}>
+          <AiOutlineClose size={25} className="absolute top-5 right-5 cursor-pointer" onClick={() => setShowMobileMenu(false)} />
+          <ul className="pt-[60px] items-center flex flex-col gap-8" >
+            <NavLink to="services" mobileMenu>Services</NavLink>
+            <NavLink to="products" mobileMenu>Shop</NavLink>
+            <NavLink to="reference" mobileMenu>Reference</NavLink>
+            <NavLink to="care" mobileMenu>Care</NavLink>
+          </ul>
+          {/* <img src={} alt="" /> */}
+        </div>
 
-  </div >;
+
+      </div>
+
+    </div >
+
+  );
+
 
 }
 
